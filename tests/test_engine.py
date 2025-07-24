c0c44b62c7967c94bfee11d8e67d5143b724f07f
OsahonDev <andrewizeiyamu@gmail.com>
2025-07-24 20:27:18 +0100

    Correcting Pylint errors

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,18 +1,18 @@
 import pytest
 from core.engine import Value
 
-@pytest.fixture
+@pytest.fixture(name="A")
 def a_value():
     a = Value(2.0)
     return a
 
-@pytest.fixture
+@pytest.fixture(name="B")
 def b_value():
     b = Value(3.0)
     return b
 
-def test_addition(a_value, b_value):
-    c = a_value + b_value
+def test_addition(A, B):
+    c = A + B
     assert c.data == 5.0, "Value doesn't add up to 5.0"
 
 

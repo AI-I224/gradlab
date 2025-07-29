24b81b454b3f71b05764c77a3c7f45f84bb9113e
OsahonDev <andrewizeiyamu@gmail.com>
2025-07-29 22:10:50 +0100

    Added docstring to tests

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -19,9 +19,12 @@ X = Value(B)
 Y = Value(C)
 Z = Value(D)
 
-TOL = 1e-07 # Tolerance to check results are accurate within the 
+TOL = 1e-07 # Tolerance to check results have error smaller than 1e-07 
 
 def test_add():
+    """
+    Tests that Value objects can be added to other Value objects, integers and floats.
+    """
     c = W + X
     d = X + W
     e = A + X
@@ -29,6 +32,9 @@ def test_add():
     assert c.data == d.data == e.data == f.data == A + B, "Operands do not add up correctly"
 
 def test_mul():
+    """
+    Tests that Value objects can be multiplied by other Value objects, integers and floats.
+    """
     c = W * X
     d = X * W
     e = A * X
@@ -40,41 +46,61 @@ def test_mul():
     (W, C)
 ])
 def test_pow(base, exponent):
+    """
+    Tests that Value objects can have an exponent using integers and floats.
+    """
     c = base ** exponent
     assert c.data == A ** exponent, "Output doesn't match the Value to the correct exponent"
 
 def test_neg():
+    """
+    Tests that Value objects have negative/reverse signs.
+    """
     c = -W
     assert c.data == -A, "Output doesn't change sign"
 
 def test_sub():
+    """
+    Tests that Value objects can be subtracted by other Value objects, integers and floats.
+    """
     c = W - X
     d = A - X
     e = W - B
     assert c.data == d.data == e.data == A - B, "Output doesn't subtract correctly"
 
-def test_rsub():
-    c = X - W
-    d = X - A
-    e = B - W
-    assert c.data == d.data == e.data == B - A, "Output doesn't subtract correctly"
-
 def test_exp():
+    """
+    Tests that Value objects can be used as an exponent of e.
+    """
     c = W.exp()
     assert abs(c.data - np.exp(A)) < TOL, "Output doesn't match exponential of A"
 
 def test_tanh():
+    """
+    Tests that .tanh() returns the output of the Value object used in the tanh function.
+    """
     c = W.tanh()
     assert abs(c.data - np.tanh(A)) < TOL, "Output doesn't match tanh(A)"
 
 def test_sigmoid():
+    """
+    Tests that .sigmoid() returns the output of the Value object used in the sigmoid function.
+    """
     c = W.sigmoid()
     assert abs(c.data - 1/(1 + np.exp(-A))) < TOL, "Output doesn't match sigmoid(A)"
 
 def test_relu_greaterthanzero():
+    """
+    Tests that .relu() returns the output of the Value object used in the relu function
+    when the Value object is greater than zero.
+    """
     c = W.relu()
     assert c.data == A, "Output doesn't match original value"
 
 def test_relu_lessthanzero():
+    """
+    Tests that .relu() returns the output of the Value object used in the relu function
+    when the Value object is less than zero.
+    """
     c = Y.relu()
     assert not c.data, "Output doesn't equal 0"

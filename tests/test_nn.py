f7404c99384e46ba109a298f9e22b322a6fc0d18
OsahonDev <andrewizeiyamu@gmail.com>
2025-08-13 12:13:21 +0100

    Added test for sequential class

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -6,16 +6,35 @@ To run tests, run the following line of code in the terminal:
 """
 
 import numpy as np
-import pytest
 from core.engine import Tensor
-from core.nn import Linear
+from core.nn import Linear, ReLU, Sequential
+
+X = Tensor(np.random.randn(3, 5).astype(np.float32))
 
 def test_linear_forward_shape():
     """
     Tests that the linear forward pass produces the correct dimension
     """
-    layer = Linear(3, 2)  # in_features=3, out_features=2
-    x = Tensor(np.random.randn(3, 4).astype(np.float32))  # (features, batch_size) = (3, 4)
-    out = layer(x)
-    assert out.data.shape == (2, 4)  # Output: (out_features, batch_size)
+    layer = Linear(3, 2)  # in_features = 3, out_features = 2
+    out = layer(X)
+    assert out.data.shape == (2, 5), "Failed producing the correcting dimensions of output Tensor"
+
+def test_sequential_forward_and_backward():
+    """
+    Tests that the multiple layers within the neural network are chain together
+    such that the forward pass and backpropagation occurs
+    """
+    model = Sequential(
+        Linear(3, 4),
+        ReLU(),
+        Linear(4, 2)
+    )
+
+    out = model(X)
+
+    loss = out.sum()
+    loss.backward()
+
+    for p in model.parameters():
+        assert p.grad.shape == p.data.shape, "The multiple layers are not chained togethe correctly"
     
